972465fbe5bc7b54845d18251d847bf833fb1e3e
Caroline Nolasco <cnolascoix@gmail.com>
2019-12-27 16:08:14 -0800

    partial controllers set up

---
+++ controllers/youtube.js
@@ -0,0 +1,14 @@
+const express       = require("express");
+const router        = express.Router();
+const Youtube       = require("../models/bookmarks.js");
+
+// Index Route
+
+// Post Route
+
+// Update Route
+
+// Delete Route
+
+module.exports = router;
+
