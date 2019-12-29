b3b23c95e5ed599d34b4b3bf0d3cd0e6ebd7c237
Caroline Nolasco <cnolascoix@gmail.com>
2019-12-29 12:58:08 -0800

    models youtube file created

--- models/youtube.js
+++ models/youtube.js
@@ -2,7 +2,9 @@ const mongoose      = require("mongoose");
 const Schema        = mongoose.Schema;
 
 const youtubeSchema = new Schema ({
-    title: String
+    title: String,
+    date: {type: Date, default: Date.now, required: true},
+    body: {type: String, required: true}
 });
 
 const Youtube = mongoose.model("Youtube", youtubeSchema);
