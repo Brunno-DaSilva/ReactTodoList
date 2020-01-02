f74bbfaffcb8ac96fa327e3c9424f4430ee2c8d6
John Martin <jmart07@gmail.com>
2020-01-02 07:57:05 -0500

    adding styling

--- public/Components/App/App.js
+++ public/Components/App/App.js
@@ -17,31 +17,45 @@ class App extends React.Component {
         <div>
           <div className="cards-container">
             <NavLink className="cards" to="/htmlCss">
+              <h1>HTML / CSS</h1>
+              <br/>
               <i class="fab fa-html5 fa-5x"></i>
               <i class="fab fa-css3 fa-5x"></i>
             </NavLink>
 
             <NavLink className="cards" to="/javaScript">
+              <h1>JavaScript</h1>
+              <br/>
               <i class="fab fa-js-square fa-5x"></i>
             </NavLink>
 
             <NavLink className="cards" to="/reactPlayList">
+              <h1>React</h1>
+              <br/>
               <i class="fab fa-react fa-5x"></i>
             </NavLink>
             <NavLink className="cards" to="/jQueryPlayList">
-              jQuery
+              <h1>jQuery</h1>
+              <br/>
             </NavLink>
             <NavLink className="cards" to="/node">
+              <h1>Node.js</h1>
+              <br/>
               <i class="fab fa-node-js fa-5x"></i>
             </NavLink>
             <NavLink className="cards" to="/phpList">
-              <i class="fab fa-js-square fa-5x"></i>
+              <h1>PHP</h1>
+              <br/>
+              <i class="fab fa-php fa-5x"></i>
             </NavLink>
             <NavLink className="cards" to="/python">
+              <h1>Python</h1>
+              <br/>
               <i class="fab fa-python fa-5x"></i>
             </NavLink>
             <NavLink className="cards" to="/codeInterview">
-              Code Interview
+              <h1>Code Interview</h1>
+              <br/>
             </NavLink>
           </div>
           <br />
