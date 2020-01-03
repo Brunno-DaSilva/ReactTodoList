a522fed367e28af60f4df3a9c076140b4a293251
John Martin <jmart07@gmail.com>
2020-01-03 17:14:11 -0500

    refactored Router.js to use map function (for styling)

--- public/Components/ReactRouter/Router.js
+++ public/Components/ReactRouter/Router.js
@@ -1,27 +1,51 @@
 const { HashRouter, NavLink, Route, browserHistory } = ReactRouterDOM;
 
+const routes = [
+  { path: '/youtubeList', component: App },
+  { path: '/about', component: About },
+  { path: '/htmlCss', component: HtmlCss },
+  { path: '/javaScript', component: JavaScript },
+  { path: '/reactPlayList', component: ReactPlayList },
+  { path: '/jQueryPlayList', component: JQueryPlayList },
+  { path: '/node', component: Node },
+  { path: '/phpList', component: Php },
+  { path: '/python', component: Python },
+  { path: '/codeInterview', component: CodeInterview },
+  { path: '/comments', component: Comments }
+];
+
 class YouTubeRouter extends React.Component {
   state = {};
   render() {
+
+    const allRoutes = routes.map(route => <Route path={ route.path } component={ route.component }></Route>);
+    console.log(allRoutes);
+
     return (
       <HashRouter history={browserHistory}>
         <Header />
-        {/* Main Components Routers */}
-        <Route path="/youtubeList" component={App} />
-        <Route path="/about" component={About} />
-
-        {/* Components Route */}
-        <Route path="/htmlCss" component={HtmlCss} />
-        <Route path="/javaScript" component={JavaScript} />
-        <Route path="/reactPlayList" component={ReactPlayList} />
-        <Route path="/jQueryPlayList" component={JQueryPlayList} />
-        <Route path="/node" component={Node} />
-        <Route path="/phpList" component={Php} />
-        <Route path="/python" component={Python} />
-        <Route path="/codeInterview" component={CodeInterview} />
-        <Route path="/comments" component={Comments} />
+          { allRoutes }
         <Footer />
       </HashRouter>
+
+      // <HashRouter history={browserHistory}>
+      //   <Header />
+      //   {/* Main Components Routers */}
+      //   <Route path="/youtubeList" component={App} />
+      //   <Route path="/about" component={About} />
+
+      //   {/* Components Route */}
+      //   <Route path="/htmlCss" component={HtmlCss} />
+      //   <Route path="/javaScript" component={JavaScript} />
+      //   <Route path="/reactPlayList" component={ReactPlayList} />
+      //   <Route path="/jQueryPlayList" component={JQueryPlayList} />
+      //   <Route path="/node" component={Node} />
+      //   <Route path="/phpList" component={Php} />
+      //   <Route path="/python" component={Python} />
+      //   <Route path="/codeInterview" component={CodeInterview} />
+      //   <Route path="/comments" component={Comments} />
+      // <Footer />
+      // </HashRouter>
     );
   }
 }
